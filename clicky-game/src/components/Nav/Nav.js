112f56e81d9ed5f260de61dfa9c784f7ffc192b3
swapnakathula <kathulaswapna@gmail.com>
2018-06-12 13:34:47 -0500

    fixed Nav bar error

--- clicky-game/src/components/Nav/Nav.js
+++ clicky-game/src/components/Nav/Nav.js
@@ -1,30 +1,4 @@
-// import React from "react";
-// import "./Nav.css";
 
-// const Nav = props => (
-//     <div>
-//    // <span>
-//     <h2><a href="/">Bird Clicky Game</a></h2>
-//     </span>
-//     <span>
-//     className={props.message.indexOf('incorrectly') !== -1 ? 
-//     "desc-incorrect" : 
-//     props.message.indexOf('correctly') !== -1 ?
-//         "desc-correct" :
-//         "desc-normal"}
-//         {props.message}
-//     </span>
-//     <span>
-//     Score: {props.curScore} | Top Score: {props.topScore}
-//     </span>
-//     <nav className="navbar navbar-dark bg-dark">
-//     <a className="navbar-brand" href="/">
-//       Clickey game
-//     </a>
-//   </nav>
-//    </div>
-    
-// );
 import React from "react";
 import "./Nav.css";
 
@@ -33,7 +7,7 @@ const Nav = props => (
         <ul className="nav nav-pills nav-justified">
             <li className="nav-item"><a href="/">Bird Clicky Game</a></li>
     
-            <li className="nav-item"
+            <li 
                 className={props.message.indexOf('incorrectly') !== -1 ? 
                     "desc-incorrect" : 
                     props.message.indexOf('correctly') !== -1 ?
