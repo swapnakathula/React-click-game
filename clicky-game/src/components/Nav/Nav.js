60725fac1134d08582e0b713dcbabc76a7ee678a
swapnakathula <kathulaswapna@gmail.com>
2018-06-12 11:03:34 -0500

    game completed

--- clicky-game/src/components/Nav/Nav.js
+++ clicky-game/src/components/Nav/Nav.js
@@ -1,11 +1,39 @@
+// import React from "react";
+// import "./Nav.css";
+
+// const Nav = props => (
+//     <div>
+//    // <span>
+//     <h2><a href="/">Bird Clicky Game</a></h2>
+//     </span>
+//     <span>
+//     className={props.message.indexOf('incorrectly') !== -1 ? 
+//     "desc-incorrect" : 
+//     props.message.indexOf('correctly') !== -1 ?
+//         "desc-correct" :
+//         "desc-normal"}
+//         {props.message}
+//     </span>
+//     <span>
+//     Score: {props.curScore} | Top Score: {props.topScore}
+//     </span>
+//     <nav className="navbar navbar-dark bg-dark">
+//     <a className="navbar-brand" href="/">
+//       Clickey game
+//     </a>
+//   </nav>
+//    </div>
+    
+// );
 import React from "react";
 import "./Nav.css";
 
 const Nav = props => (
     <div>
         <ul className="nav nav-pills nav-justified">
-            <li><a href="/">Bird Clicky Game</a></li>
-            <li
+            <li className="nav-item"><a href="/">Bird Clicky Game</a></li>
+    
+            <li className="nav-item"
                 className={props.message.indexOf('incorrectly') !== -1 ? 
                     "desc-incorrect" : 
                     props.message.indexOf('correctly') !== -1 ?
@@ -14,7 +42,7 @@ const Nav = props => (
             >
                 {props.message}
             </li>
-            <li>Score: <span style={{color: "yellow"}}>{props.curScore}</span> | Top Score: {props.topScore}</li>
+            <li className="nav-item">Score: <span>{props.curScore}</span> | Top Score: {props.topScore}</li>
         </ul>
     </div>
 );
